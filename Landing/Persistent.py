6bc3ad283b19be79beabedb39feb574f39fd4fe4
Max Tico <maxtico@Maxs-MacBook-Pro.local>
2023-10-26 19:06:08 +0200

    Persistent

--- Landing/Persistent.py
+++ Landing/Persistent.py
@@ -11,6 +11,10 @@ def Persistent(filename, source_folder, destination_folder):
         return
     user_choice = input(f"Do you want to move '{filename}' to the '{destination_folder}' folder? (0/1): ")
     
+    if not os.path.exists(destination_folder):
+        # Create the destination folder if it doesn't exist
+        os.makedirs(destination_folder)
+    
     if user_choice == '1':
         # Get the current date in the desired format (e.g., YYYY-MM-DD)
         current_date = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -18,21 +22,12 @@ def Persistent(filename, source_folder, destination_folder):
         # Split the filename into its name and extension
         name, extension = os.path.splitext(filename)
 
-        # Construct the new filename with the date
-        new_filename = f"{name}_{current_date}{extension}"
-
         # Build the full destination path with the new filename
-        destination_path_with_date = os.path.join(destination_folder, new_filename)
+        new_filename = os.path.join(destination_folder, f"{name}_{current_date}{extension}")
+        os.rename(source_path,new_filename)
 
         # Move the file to the destination folder with the new filename
-        shutil.move(source_path, destination_path_with_date)
-        print(f"'{filename}' has been moved to '{destination_folder}' as '{new_filename}'.")
+        #shutil.move(source_path, new_filename)
+        print(f"'{filename}' has been moved to '{destination_folder}' as '{os.path.basename(new_filename)}'.")
     else:
         print("Operation canceled by the user.")
-
-
-# Example usage:
-source_folder = "/Users/maxtico/Documents/Master Data Science/ADSDB/ADSDB_Project/"
-destination_folder = "./"
-filename = "owid-covid-data_v1.csv"
-Persistent(filename, source_folder, destination_folder)
