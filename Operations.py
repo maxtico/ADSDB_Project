4f1b6c1a8c9a3585c6772e2aa6d3146fdb9ab359
Max Tico <maxtico@Maxs-MacBook-Pro.local>
2023-10-26 16:13:04 +0200

    Operations almost finished

--- Operations.py
+++ Operations.py
@@ -2,15 +2,26 @@ from Landing.Persistent import Persistent
 from Formatted.Formatted import Formatted
 from Trusted.Owid.Owid_trusted import Owid_trusted
 from Trusted.Worldometer.Worldometer_trusted import Worldometer_trusted
+import warnings
+import os
 
 def main():
-    source_folder = "/Users/maxtico/Documents/Master Data Science/ADSDB/ADSDB_Project/Landing/Temporal/"
-    destination_folder = "/Users/maxtico/Documents/Master Data Science/ADSDB/ADSDB_Project/Landing/Persistent/"
-    filename = "/Users/maxtico/Documents/Master Data Science/ADSDB/ADSDB_Project/Landing/Temporal/owid-covid-data_v1.csv"
-    Persistent(filename,source_folder,destination_folder)
-    Formatted()
-    Owid_trusted()
-    Worldometer_trusted()
+    rel_path = os.path.dirname(os.path.abspath(__name__))
+    source_folder = rel_path+"/Landing/Temporal/"
+    destination_folder = rel_path+"/Landing/Persistent/"
+    filename = ["owid-covid-data_v1.csv","owid-covid-data_v2.csv","worldometer_v1.csv", "worldometer_v2.csv"]
+    print('\n.....Executing Landing zone.....')
+    for file in filename:
+        Persistent(source_folder+file,source_folder,destination_folder)
+    print('\n.....Executing Formatted zone.....')
+    Formatted(rel_path)
+    print('\n.....Executing Trusted zone.....')
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        print('\n.....Performing quality analysis & treating missing data for Owid dataset.....')
+        Owid_trusted()
+        print('\n.....Performing quality analysis & treating missing data for Worldometer dataset.....')
+        Worldometer_trusted()
 
 
 if __name__ == "__main__":
