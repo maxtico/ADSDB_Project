0e216c384cc5fb40aa266e1f64a8d59da5bc6d60
maxtico <104825030+maxtico@users.noreply.github.com>
2024-01-12 12:33:11 +0100

    Formatted

---
+++ DataManagementBackbone/Formatted/Formatted.py
@@ -0,0 +1,36 @@
+import duckdb
+import glob
+import time
+import os
+import pandas as pd
+
+def Formatted(filepath):
+    conn = duckdb.connect()
+    # Display our tables in duckdb
+    file_list = os.listdir(filepath)
+    # Initialize dataframes to store the data
+    owid_dataframes = []
+    worldometer_dataframes = []
+    for file in file_list:
+        if file.endswith(".csv"):
+            # Check if the file name contains "owid" or "worldometer"
+            if "owid-covid" in file:
+                owid_dataframes.append(file)
+            elif "worldometer" in file:
+                worldometer_dataframes.append(file)
+    
+    # Joining the datasets: Worldometer
+    w1 = worldometer_dataframes[0]
+    w2 = worldometer_dataframes[1]
+    worldometer_complete = conn.execute(f"SELECT * FROM read_csv_auto(['{filepath}{w1}','{filepath}{w2}'], union_by_name=true)").df()
+    output_file = os.path.join(filepath, 'Worldometer_joined.csv')
+    worldometer_complete.to_csv(output_file)  # or True?
+
+
+    # Joining the datasets: Owid
+    o1 = owid_dataframes[0]
+    o2 = owid_dataframes[1]
+    owid_complete = conn.execute(f"SELECT * FROM read_csv_auto(['{filepath}{o1}','{filepath}{o2}'], union_by_name=true)").df()
+    output_file = os.path.join(filepath, 'Owid_joined.csv')
+    owid_complete.to_csv(output_file)  # or True?
+
